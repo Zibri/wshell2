5eaf3fe82bf26191b645a7dd66e395f2411986f4
Zibri <Zibri@users.noreply.github.com>
2024-01-04 02:26:24 +0000

    Update 7 files

--- config-replit.js
+++ config-replit.js
@@ -1,17 +1,16 @@
 var config = {
+  server: {
+    debug: false,
+    port: 3000,
+    host: null, // "127.0.0.1", "0.0.0.0", "x.y.z.k"
+    allowed: ["*"], // allowed origins array
+  },
 
-    server: {
-        debug: false,
-        port: 3000,
-        host: null, // "127.0.0.1", "0.0.0.0", "x.y.z.k"
-        allowed: ["*"] // allowed origins array
-    },
-
-    client: {
-        debug: false,
-        host: "https://wshell2.zibri.repl.co",
-        io: "wss://wshell2.zibri.repl.co" // or "" or "ws://localhost" or anything else
-    }
+  client: {
+    debug: false,
+    host: "https://yvrzd3-3000.csb.app",
+    io: "wss://yvrzd3-3000.csb.app", // or "" or "ws://localhost" or anything else
+  },
 };
 
-module.exports = config
+module.exports = config;
